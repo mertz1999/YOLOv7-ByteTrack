bf2695d75c84d3139f83c4986e55a254fed089cf
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-11-03 23:31:43 +0330

    (REFACTOR)

--- run.py
+++ run.py
@@ -24,12 +24,30 @@ with torch.no_grad():
         # Detections 
         dets = yolo.predict(frame)
 
-        for row in dets[0]:
-            plot_one_box((row[0],row[1],row[2],row[3]), frame, label=yolo.names[int(row[5])], color=yolo.colors[int(row[5])], line_thickness=1)
+        # for row in dets[0]:
+        #     plot_one_box((row[0],row[1],row[2],row[3]), frame, label=yolo.names[int(row[5])], color=yolo.colors[int(row[5])], line_thickness=1)
         
-        cv2.imshow("result",frame)
+        # cv2.imshow("result",frame)
 
         #
         # online_targets = tracker.update(torch.tensor(dets[0]), (640,640), (640,640))
         # print(online_targets)
         # print(tracker.update(dets, ))
+
+        # run tracking
+        if dets[0] is not None:
+            online_targets = tracker.update(torch.tensor(dets[0]), (640,640), (640,640))
+            online_tlwhs = []
+            online_ids = []
+            online_scores = []
+            for t in online_targets:
+                tlwh = t.tlwh
+                tid = t.track_id
+                vertical = tlwh[2] / tlwh[3] > 1.6
+                if tlwh[2] * tlwh[3] > 0.1 and not vertical:
+                    online_tlwhs.append(tlwh)
+                    online_ids.append(tid)
+                    online_scores.append(t.score)
+            # save results
+            print(frame_id, online_tlwhs, online_ids, online_scores)
+
