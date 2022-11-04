210c17e2215e4a3ced03091169edf5c878b02c1b
Reza Tanakizadeh <reza.tz780210@gmail.com>
2022-11-05 01:26:54 +0330

    (REFACTOR)

--- run.py
+++ run.py
@@ -7,13 +7,19 @@ from byte.byte_tracker import BYTETracker
 from yolov7.utils.plots import plot_one_box
 
 
-# Output Video
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')  
-out_vid = cv2.VideoWriter("result_viedeo.mp4", fourcc, 30, (640, 640),True)
 
 with torch.no_grad():
     # Read Video capture
-    cap = cv2.VideoCapture('1_02.mp4')
+    cap    = cv2.VideoCapture('1_02.mp4')
+    width  = cap. get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = cap. get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    x_scale = width / 640
+    y_scale = height / 640
+
+    # Output Video
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')  
+    out_vid = cv2.VideoWriter("result_viedeo.mp4", fourcc, 30, (x_scale, y_scale),True)
 
     # Make instance of yolov7 and trackers
     yolo = DetectObjects('./yolov7.pt',640,0.25, device_name='cuda')
@@ -21,17 +27,11 @@ with torch.no_grad():
 
     while(cap.isOpened()):
         # Capture frame-by-frame
-        ret, frame = cap.read() 
-        frame = cv2.resize(frame, (640,640))  
+        ret, org_frame = cap.read() 
+        frame = cv2.resize(org_frame, (640,640))  
 
         # Detections 
         dets = yolo.predict(frame)
-
-        # Save for Detection frame
-        # for row in dets[0]:
-        #     plot_one_box((row[0],row[1],row[2],row[3]), frame, label=yolo.names[int(row[5])], color=yolo.colors[int(row[5])], line_thickness=1)
-        # cv2.imwrite('./detection.jpg', frame)
-
 
         # run tracking
         if dets[0] is not None:
@@ -50,8 +50,12 @@ with torch.no_grad():
         
         # Save Traking output as image frame
         for idx, row in enumerate(online_tlwhs):
-            plot_one_box((row[0],row[1],row[0]+row[2],row[1]+row[3]), frame, label=str(online_ids[idx]), color=yolo.colors[2], line_thickness=1)
-        # cv2.imwrite('./tracker.jpg', frame)
+            plot_one_box(
+                         (row[0]*x_scale, row[1]*y_scale, (row[0]+row[2])*x_scale, (row[1]+row[3])*x_scale),
+                         org_frame, label=str(online_ids[idx]), 
+                         color=yolo.colors[2], 
+                         line_thickness=1
+                         )
         
         out_vid.write(frame)
         # exit()
